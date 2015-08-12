5ac631591f8b810debd4b19a02d3f60907b7d14a
Nicholas Udell <nick@udellgames.com>
2015-08-12 20:41:36 +0100

    Migrated normal map generation to separate library to separate view projects from the actual logic.

--- NormalizerWPF/MainWindow.xaml.cs
+++ NormalizerWPF/MainWindow.xaml.cs
@@ -71,36 +71,10 @@ namespace NormalMapMaker
             if (ofd.ShowDialog() == true)
             {
                 before = new Bitmap(ofd.FileName);
-                after = CreateNormal(before);
+                after = NormalizrCore.Normalizr.CreateNormal(before);
                 LoadedImage.Source = ToBitmapImage(before);
                 SavedImage.Source = ToBitmapImage(after);
             }
-        }
-
-        private Bitmap CreateNormal(Bitmap image)
-        {
-            var result = new Bitmap(image);
-            for(var x= 0; x< image.Width ; x++)
-            {
-                
-                for(var y=0; y< image.Height; y++)
-                {
-                    if (x == 0 || x == image.Width - 1 || y == 0 || y == image.Height - 1)
-                    {
-                        result.SetPixel(x, y, System.Drawing.Color.FromArgb(image.GetPixel(x,y).A,System.Drawing.Color.Blue));
-                    }
-                    else
-                    {
-                        var horizontalChange = (image.GetPixel(x + 1, y).GetBrightness() - image.GetPixel(x - 1, y).GetBrightness()); // how much the pixel to the right is higher than the one to the left.
-                        byte r = (byte)(127 + horizontalChange * 128);
-                        var verticalChange = (image.GetPixel(x, y + 1).GetBrightness() - image.GetPixel(x, y - 1).GetBrightness()); // how much the pixel to the bottom is higher than the one to the top.
-                        byte g = (byte)(127 + verticalChange * 128);
-                        byte b = (byte)(255 - (Math.Abs(horizontalChange) + Math.Abs(verticalChange)) * 255);
-                        result.SetPixel(x, y, System.Drawing.Color.FromArgb(image.GetPixel(x, y).A, r, g, b));
-                    }
-                }
-            }
-            return result;
         }
     }
 }
---
+++ NormalizrCore/Normalizr.cs
@@ -0,0 +1,32 @@
+﻿namespace NormalizrCore
+{
+    using System;
+    using System.Drawing;
+    public static class Normalizr
+    {
+        public static Bitmap CreateNormal(Bitmap image)
+        {
+            var result = new Bitmap(image);
+            for (var x = 0; x < image.Width; x++)
+            {
+                for (var y = 0; y < image.Height; y++)
+                {
+                    if (x == 0 || x == image.Width - 1 || y == 0 || y == image.Height - 1)
+                    {
+                        result.SetPixel(x, y, System.Drawing.Color.FromArgb(image.GetPixel(x, y).A, System.Drawing.Color.Blue));
+                    }
+                    else
+                    {
+                        var horizontalChange = (image.GetPixel(x + 1, y).GetBrightness() - image.GetPixel(x - 1, y).GetBrightness()); // how much the pixel to the right is higher than the one to the left.
+                        var r = (byte)(127 + horizontalChange * 128);
+                        var verticalChange = (image.GetPixel(x, y + 1).GetBrightness() - image.GetPixel(x, y - 1).GetBrightness()); // how much the pixel to the bottom is higher than the one to the top.
+                        var g = (byte)(127 + verticalChange * 128);
+                        var b = (byte)(255 - (Math.Abs(horizontalChange) + Math.Abs(verticalChange)) * 255);
+                        result.SetPixel(x, y, Color.FromArgb(image.GetPixel(x, y).A, r, g, b));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
